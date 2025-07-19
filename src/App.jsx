8665fd555bb07b4f26a30fe13ab6f330528e6592
jacquikeating <j.keating@live.ca>
2025-07-19 11:36:19 -0400

    Added <Status> component
    - Created <Status> component.
    - Filled out <Status> structure: an <h2> and a <p>, both to indicate to the user whether they have won/lost the overall game, or to provide status messages as they play.
    - Temporarily using hardcoded values for scaffolding purposes.
    - Imported <Status> into <App>.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Status from './components/Status'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Status />
     </>
   )
 }
